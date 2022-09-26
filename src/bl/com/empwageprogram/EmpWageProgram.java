3f245ca828a969084b89555077a9832e9d100342
amod1811 <ammch101@gmail.com>
2022-09-26 12:51:29 +0530

    Added the program to calculate daily Emp wage

--- src/bl/com/empwageprogram/EmpWageProgram.java
+++ src/bl/com/empwageprogram/EmpWageProgram.java
@@ -3,16 +3,26 @@ package bl.com.empwageprogram;
 public class EmpWageProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Program");
-		
-		int fulltime = 1;
-		double check =  Math.floor(Math.random() * 10) %2;
-		
-		if(check == fulltime){
-			System.out.println("Emp is present");
-		}
-		else {
-			System.out.println("Emp is not present");
+		//variables
+				int fullTime =1;
+				int  empHrs=8;
+				int Wsa =0;
+				int emp_Rate_Per_Hour = 20;
+				int empWage = 20;
+				
+				//use random method check employee is present or not
+				double empCheck = Math.floor(Math.random()* 10)%2;
+				if (empCheck == fullTime)
+					System.out.println("Employee is present");
+				else
+					System.out.println("Employee is not prsent");
+				
+				//calculate employee wage 
+				if (empCheck == fullTime)
+					empHrs = 12;
+				empWage = empHrs * emp_Rate_Per_Hour;
+				System.out.println("Emp Wage : " + empWage);
+						
 		}
 	}
-}
+
