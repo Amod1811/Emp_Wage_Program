e9c959a1c101547a710be0ce4912a781d546d4e9
amod1811 <ammch101@gmail.com>
2022-09-26 12:58:35 +0530

    Added the program to calculate daily Emp wage to use part time

--- src/bl/com/empwageprogram/EmpWageProgram.java
+++ src/bl/com/empwageprogram/EmpWageProgram.java
@@ -4,25 +4,29 @@ public class EmpWageProgram {
 
 	public static void main(String[] args) {
 		//variables
-				int fullTime =1;
+				int is_full_time =1;
+				int is_part_time = 2;
 				int  empHrs=8;
 				int Wsa =0;
-				int emp_Rate_Per_Hour = 20;
+				int emp_rate_per_hour = 20;
 				int empWage = 20;
 				
 				//use random method check employee is present or not
-				double empCheck = Math.floor(Math.random()* 10)%2;
-				if (empCheck == fullTime)
+				double empCheck = Math.floor(Math.random()* 10)%3;
+				if (empCheck == is_full_time)
 					System.out.println("Employee is present");
 				else
-					System.out.println("Employee is not prsent");
+					System.out.println("Employee is not present");
 				
 				//calculate employee wage 
-				if (empCheck == fullTime)
+				if (empCheck == is_full_time)
 					empHrs = 12;
-				empWage = empHrs * emp_Rate_Per_Hour;
+				else if (empCheck == is_part_time)
+					empHrs = 8;
+				else
+					empHrs = 0;
+				empWage = empHrs * emp_rate_per_hour;
 				System.out.println("Emp Wage : " + empWage);
-						
 		}
 	}
 
