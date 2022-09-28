71151f144b2201a8a3c8ba43739fb25ac122bc5f
amod1811 <ammch101@gmail.com>
2022-09-28 08:00:21 +0530

    Added program to calculate Employee Wage for Month

--- EmpWageProgram.java
+++ EmpWageProgram.java
@@ -1,5 +1,6 @@
 package bl.com.empwageprogram;
 public class EmpWageProgram {
+
 	public static final int IS_PART_TIME = 1;
 	public static  final int IS_FULL_TIME = 2;
 	public static final int  EMP_RATE_PER_HOUR = 20;
